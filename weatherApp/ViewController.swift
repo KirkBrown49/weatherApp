a987d1ace7dd798c26ba89a5c54c1a1b0659602a
Kirk Brown <kirkbrown@macbook-pro-97.local>
2018-11-14 12:31:44 -0600

    Initial Commit

---
+++ weatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weatherApp
+//
+//  Created by Kirk Brown on 11/14/18.
+//  Copyright © 2018 Kirk Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
